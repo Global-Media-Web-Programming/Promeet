90eb72d9e7631555c2ff3a41102e801134f4ca54
duwlsssss <rladuwls0814@gmail.com>
2025-05-20 10:17:51 +0900

    style: outlet 양옆 패딩 추가

--- src/components/Test.styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { theme } from '@/styles/theme';
-
-export const Header = styled.header`
-  color: ${theme.color.main};
-`;
---
+++ src/components/map/MapContainer/index.jsx
@@ -0,0 +1,7 @@
+import * as S from 'style';
+
+const MapContainer = () => {
+  return <S.MapDiv id="map" style="width:500px;height:400px;"></S.MapDiv>;
+};
+
+export default MapContainer;
