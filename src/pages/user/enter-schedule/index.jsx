b25e8c368d936a30c90c5f313cfa39d61405e44f
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-05-26 01:15:38 +0900

    test: 테스트 모달 생성

--- src/pages/user/enter-schedule/index.jsx
+++ src/pages/user/enter-schedule/index.jsx
@@ -1,6 +1,22 @@
-import * as S from './style';
+import { useState } from 'react';
+// import * as S from './style';
+import CommonModal from '@/components/modal/CommonModal';
+import addIcon from '@/assets/img/icon/add.svg';
 
 const UserPage = () => {
-  return <S.Container>스케줄 입력 페이지</S.Container>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>
+        <img src={addIcon} alt="Add" />
+      </button>
+      <CommonModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <h2>테스트 모달</h2>
+        <p>이곳에 원하는 내용을 넣으세요.</p>
+      </CommonModal>
+    </>
+  );
 };
+
 export default UserPage;
