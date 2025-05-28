e307f2944f2c5583393af785f6140d7a59ae1716
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-05-28 18:42:23 +0900

    feat: 고정 일정 시간표 기능 구현

--- src/pages/user/enter-schedule/index.jsx
+++ src/pages/user/enter-schedule/index.jsx
@@ -1,6 +1,12 @@
 import * as S from './style';
+import FixedTimeTable from '@/components/timeTable/FixedTimeTable';
 
 const UserPage = () => {
-  return <S.Container>스케줄 입력 페이지</S.Container>;
+  return (
+    <>
+      <S.Container>스케줄 입력 페이지</S.Container>
+      <FixedTimeTable />
+    </>
+  );
 };
 export default UserPage;
