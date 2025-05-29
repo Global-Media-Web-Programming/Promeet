52d55b2862a732c940c08a7e1850607a9a5cedc8
minji2424 <wendymin713@gmail.com>
2025-05-29 16:57:44 +0900

    test: 버튼 테스트 추가

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,6 +1,15 @@
 import * as S from './style';
+import LoginButton from '@/components/ui/Button';
+// import InputBox from '@/components/ui/InputBox';
 
 const SignInPage = () => {
-  return <S.Container>로그인 페이지</S.Container>;
+  return (
+    <S.Container>
+      <S.Title>로그인</S.Title>
+
+      <LoginButton text="로그인" />
+    </S.Container>
+  );
 };
+
 export default SignInPage;
--- src/pages/sign-in/style.js
+++ src/pages/sign-in/style.js
@@ -2,5 +2,20 @@ import styled from 'styled-components';
 import { theme } from '@/styles/theme';
 
 export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  min-height: 100vh;
+  padding: 40px 16px;
+
+  background: ${theme.color.white};
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 32px;
+  font-size: 2rem;
+  font-weight: 700;
   color: ${theme.color.point1};
 `;
