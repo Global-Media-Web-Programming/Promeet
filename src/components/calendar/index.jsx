6742165067754bc2bebea27a9975e29dadf7880c
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-05-31 01:49:17 +0900

    feat: 캘린더 기능 구현

--- src/components/calendar/index.jsx
+++ src/components/calendar/index.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
-import * as S from './style.js';
+import * as S from './style';
 import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
+import { ko } from 'date-fns/locale';
+
+// 요일 이름 커스터마이징
+const customLocale = {
+  ...ko,
+  localize: {
+    ...ko.localize,
+    day: (n) => ['S', 'M', 'T', 'W', 'T', 'F', 'S'][n],
+  },
+};
 
 export default function CalendarRange() {
   const [range, setRange] = useState({ from: undefined, to: undefined });
 
-  // 제목 포맷 함수
-  const formatCaption = (date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // 월을 2자리로
-    return `${year}.${month}`;
-  };
+  const formatCaption = (date) => format(date, 'yyyy.MM', { locale: customLocale });
 
   return (
     <S.CalendarWrapper>
@@ -19,12 +25,15 @@ export default function CalendarRange() {
         mode="range"
         selected={range}
         onSelect={setRange}
-        disabled={{ before: new Date() }}
-        weekStartsOn={0}
-        formatters={{ formatCaption }} // 제목 포맷 함수 적용
         navLayout="around"
-        fixedWeeks={true}
-        showOutsideDays={true}
+        weekStartsOn={0}
+        disabled={{ before: new Date() }}
+        fixedWeeks
+        showOutsideDays
+        locale={customLocale}
+        formatters={{
+          formatCaption,
+        }}
       />
     </S.CalendarWrapper>
   );
