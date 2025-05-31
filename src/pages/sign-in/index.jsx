a34097d24043696d92646151c29c2b63199f7176
minji2424 <wendymin713@gmail.com>
2025-05-31 18:15:49 +0900

    feat: 로그인 페이지 구현

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,6 +1,48 @@
+import React, { useState } from 'react';
 import * as S from './style';
+import LoginButton from '@/components/ui/Button';
+import InputBox from '@/components/ui/InputBox';
 
 const SignInPage = () => {
-  return <S.Container>로그인 페이지</S.Container>;
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <S.Container>
+      {/* 상단 상태바 */}
+      {/*<S.StatusBar>
+        <S.TimeText>9:41</S.TimeText>
+        <S.StatusDot />
+      </S.StatusBar>*/}
+
+      {/* 로그인 타이틀 */}
+      <S.Title>로그인</S.Title>
+
+      {/* 이름 인풋 */}
+      <S.InputWrapper top="267px">
+        <InputBox
+          placeholder="이름을 입력해주세요"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </S.InputWrapper>
+
+      {/* 비밀번호 인풋 */}
+      <S.InputWrapper top="357px">
+        <InputBox
+          placeholder="비밀번호를 입력해주세요"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+        />
+      </S.InputWrapper>
+
+      {/* 버튼 */}
+      <S.ButtonWrapper>
+        <LoginButton text="로그인하기" />
+      </S.ButtonWrapper>
+    </S.Container>
+  );
 };
+
 export default SignInPage;
