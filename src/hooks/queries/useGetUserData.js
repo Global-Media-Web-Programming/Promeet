91ba52c216f51f728a7114a31fb94bfe9a0c6fcd
duwlsssss <rladuwls0814@gmail.com>
2025-05-31 22:23:21 +0900

    refactor: 로그인 onSuccess 제거

--- src/hooks/queries/useGetUserData.js
+++ src/hooks/queries/useGetUserData.js
@@ -1,33 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUserData } from '@/apis/get/auth';
 import { QUERY_KEY } from '@/constants/key';
-import useHandleError from '../useHandleError';
 import { useUserActions } from '../stores/auth/useUserStore';
 
-// 유저 id로 유저 정보 가져오기
 const useGetUserData = (userId) => {
-  const handleError = useHandleError();
-  console.log('useGetUserData 실행');
   const { setUserName, setFixedSchedules, setPromises } = useUserActions();
 
   return useQuery({
     queryKey: [QUERY_KEY.user, userId],
-    queryFn: () => getUserData(userId),
-    enabled: !!userId,
-    onSuccess: (data) => {
-      console.log('useGetUserData onSuccess');
-      const { name, fixedSchedule, promise } = data.data;
-
+    queryFn: async () => {
+      const { data } = await getUserData(userId);
+      // 데이터를 가져오자마자 store 업데이트
+      const { name, fixedSchedule, promise } = data;
       setUserName(name);
       setFixedSchedules(fixedSchedule);
       setPromises({
         create: promise?.create ?? [],
         join: promise?.join ?? [],
       });
+      return data;
     },
-    onError: (error) => {
-      handleError(error);
-    },
+    enabled: !!userId,
   });
 };
 
