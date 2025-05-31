008dbf219acaf608a117aacc0d6cf36c532255dd
minji2424 <wendymin713@gmail.com>
2025-05-31 19:51:48 +0900

    feat(button): 버튼 컴포넌트 구현

--- src/components/ui/Button/index.jsx
+++ src/components/ui/Button/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 /**
  * Button 컴포넌트
  *
- * @param {string} text - 버튼 글자
+ * @property {string} text - 버튼 글자
  */
 
 const Button = ({ text }) => {
@@ -15,4 +15,44 @@ Button.propTypes = {
   text: PropTypes.string.isRequired,
 };
 
-export default Button;
+const LoginButton = ({ text }) => {
+  return (
+    <S.ButtonWrapper>
+      <S.ButtonBackground />
+      <S.ButtonText>{text}</S.ButtonText>
+    </S.ButtonWrapper>
+  );
+};
+
+LoginButton.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+const DisabledButton = ({ text }) => {
+  return (
+    <S.DisabledButtonWrapper disabled>
+      <S.DisabledButtonBackground />
+      <S.DisabledButtonText>{text}</S.DisabledButtonText>
+    </S.DisabledButtonWrapper>
+  );
+};
+
+DisabledButton.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+const StartButton = ({ text }) => {
+  return (
+    <S.StartButtonWrapper disabled>
+      <S.StartButtonBackground />
+      <S.StartButtonText>{text}</S.StartButtonText>
+    </S.StartButtonWrapper>
+  );
+};
+
+StartButton.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default LoginButton;
+export { DisabledButton, Button, StartButton };
