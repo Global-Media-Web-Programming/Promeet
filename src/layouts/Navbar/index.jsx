47dd9ce951db52c4deb11aaba53e392ff945d6d4
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-06-10 16:23:33 +0900

    chore: pathname 수정

--- src/layouts/Navbar/index.jsx
+++ src/layouts/Navbar/index.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import { Home, User } from 'lucide-react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { BottomBar, AddButton, NavButton } from './style';
+import { ROUTES } from '@/constants/routes';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   // signin 화면일 때는 null 리턴해서 안보이게
-  if (location.pathname === '/sign-in') {
+  if (location.pathname === ROUTES.SIGN_IN) {
     return null;
   }
 
   return (
     <BottomBar>
-      <NavButton active={location.pathname === '/'} onClick={() => navigate('/')}>
+      <NavButton active={location.pathname === ROUTES.HOME} onClick={() => navigate(ROUTES.HOME)}>
         <Home />
       </NavButton>
 
-      <AddButton onClick={() => navigate('/promise/create-info')}>+</AddButton>
+      <AddButton onClick={() => navigate(ROUTES.PROMISE_CREATE_INFO)}>+</AddButton>
 
-      <NavButton active={location.pathname === '/user'} onClick={() => navigate('/user')}>
+      <NavButton active={location.pathname === ROUTES.USER} onClick={() => navigate(ROUTES.USER)}>
         <User />
       </NavButton>
     </BottomBar>
