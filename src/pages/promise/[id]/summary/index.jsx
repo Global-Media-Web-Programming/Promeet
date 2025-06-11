b648dc5aa8608ef1b63f42c3cd80b8a0c9125001
duwlsssss <rladuwls0814@gmail.com>
2025-06-11 17:30:47 +0900

    feat: summary page 지도 추가

--- src/pages/promise/[id]/summary/index.jsx
+++ src/pages/promise/[id]/summary/index.jsx
@@ -1,18 +1,15 @@
 import * as S from './style';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useState } from 'react';
 import Header from '@/components/promise/Header';
 import { usePromiseDataFromServerInfo } from '@/hooks/stores/promise/usePromiseDataFromServerStore';
 import { PROMISE_JOIN_HEADER_TEXT } from '@/constants/promise';
-import { BUILD_ROUTES } from '@/constants/routes';
 import copyToClipboard from '@/utils/copyToClipBoard';
 import toast from '@/utils/toast';
+import FinalPlaceMap from '@/components/promise/map/FinalPlaceMap';
 
 const SummaryPage = () => {
   const { promiseDataFromServer } = usePromiseDataFromServerInfo();
-
-  const navigate = useNavigate();
-  const { promiseId } = useParams();
-
+  const [isMapOpen, setIsMapOpen] = useState(false);
   // const time = promiseDataFromServer.fixedTime
   //   .map(({ date, startTime, endTime }) => {
   //     const [year, month, day] = date.split('-');
@@ -27,11 +24,11 @@ const SummaryPage = () => {
   const place = '상도 곱창\n서울시 동작구 232';
   const dummyPlace = {
     placeId: '1313432',
-    type: 'restuarant', // or "cafe" or "studyCafe" or "activity"
+    type: 'restaurant',
     name: '상도 곱창',
     position: {
-      La: '37.49808633653005',
-      Ma: '127.02800140627488',
+      Ma: 37.49808633653005,
+      La: 127.02800140627488,
     },
     address: '서울시 동작구 232',
     phone: '02-123-1234', // 선택
@@ -45,42 +42,43 @@ const SummaryPage = () => {
   };
 
   const handlePlaceMapClick = () => {
-    navigate(BUILD_ROUTES.PROMISE_MAP(promiseId));
+    setIsMapOpen(!isMapOpen);
   };
 
   return (
-    <>
-      <S.Container>
-        <Header text={PROMISE_JOIN_HEADER_TEXT} />
-        <S.InfoConainer>
-          <S.Name>{promiseDataFromServer.title}</S.Name>
-          <S.Description>{promiseDataFromServer.description}</S.Description>
-        </S.InfoConainer>
-        <S.Line />
-        <S.StatusContainer>
-          <S.StatusWrapper>
-            <S.TimeIcon />
-            {time}
-          </S.StatusWrapper>
-          <S.StatusWrapper>
-            <S.LocationIcon />
-            {place}
-            <S.CopyIcon onClick={handleCopyClick} />
-            <S.MapIcon onClick={handlePlaceMapClick} />
-          </S.StatusWrapper>
-          <S.StatusWrapper>
-            <S.PeopleIcon />
-            <S.MemberList>
-              {promiseDataFromServer.members.map((member) => (
-                <S.MemberItem key={member.userId}>
-                  <p>{member.name}</p>
-                </S.MemberItem>
-              ))}
-            </S.MemberList>
-          </S.StatusWrapper>
-        </S.StatusContainer>
-      </S.Container>
-    </>
+    <S.Container>
+      <Header text={PROMISE_JOIN_HEADER_TEXT} />
+      <S.InfoConainer>
+        <S.Name>{promiseDataFromServer.title}</S.Name>
+        <S.Description>{promiseDataFromServer.description}</S.Description>
+      </S.InfoConainer>
+      <S.Line />
+      <S.StatusContainer>
+        <S.StatusWrapper>
+          <S.TimeIcon />
+          {time}
+        </S.StatusWrapper>
+        <S.StatusWrapper>
+          <S.LocationIcon />
+          {place}
+          <S.CopyIcon onClick={handleCopyClick} />
+          <S.MapIcon onClick={handlePlaceMapClick} />
+        </S.StatusWrapper>
+        <S.StatusWrapper>
+          <S.PeopleIcon />
+          <S.MemberList>
+            {promiseDataFromServer.members.map((member, index) => (
+              <p key={index}>{member.name}</p>
+            ))}
+          </S.MemberList>
+        </S.StatusWrapper>
+      </S.StatusContainer>
+      {isMapOpen ? (
+        <S.MapSection>
+          <FinalPlaceMap place={dummyPlace} />
+        </S.MapSection>
+      ) : null}
+    </S.Container>
   );
 };
 
