3d672a3cbc3e6335ffd8e9aa20bfda45ae9dddb4
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-06-11 10:10:35 +0900

    feat: join 페이지 구현

--- src/pages/promise/[id]/join/index.jsx
+++ src/pages/promise/[id]/join/index.jsx
@@ -1,6 +1,19 @@
 import * as S from './style';
+import SignInForm from '@/components/auth/SignInForm';
 
 const JoinPage = () => {
-  return <S.Container>참여자 로그인 페이지</S.Container>;
+  return (
+    <S.Container>
+      <S.JoinTitle>
+        OOO 님이
+        <br />
+        약속을 공유했습니다.
+      </S.JoinTitle>
+      <S.PromiseName>약속 제목</S.PromiseName>
+      <S.PromiseDescription>약속 내용</S.PromiseDescription>
+      <S.Divider />
+      <SignInForm />
+    </S.Container>
+  );
 };
 export default JoinPage;
--- src/pages/promise/[id]/join/style.js
+++ src/pages/promise/[id]/join/style.js
@@ -2,5 +2,43 @@ import styled from 'styled-components';
 import { theme } from '@/styles/theme';
 
 export const Container = styled.section`
-  color: ${theme.color.point1};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const JoinTitle = styled.h1`
+  font-family: Pretendard, sans-serif;
+  font-size: 36px;
+  font-weight: 600;
+  font-style: normal;
+  color: ${theme.color.text.blue};
+`;
+
+export const PromiseName = styled.h2`
+  margin-top: 50px;
+
+  font-family: Pretendard, sans-serif;
+  font-size: 30px;
+  font-weight: 600;
+  font-style: normal;
+  color: ${theme.color.text.black};
+`;
+
+export const PromiseDescription = styled.p`
+  margin-top: 25px;
+
+  font-family: Pretendard, sans-serif;
+  font-size: 18px;
+  font-weight: 500;
+  font-style: normal;
+  color: ${theme.color.text.black};
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 11px;
+  margin: 30px 0;
+  background-color: #e9e9e9;
 `;
